3d33b94a606fb7a3d8d3517ab7dac3c7ff189a92
Abby Blachman <abbyblachman@Abbys-MacBook-Pro.local>
2020-03-11 17:54:49 -0500

    fixing bug in server

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.get('*', (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+}
 
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
