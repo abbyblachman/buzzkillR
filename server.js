82ed19d5fef565fc87f8071e91cfa2ba9aa5d7de
Abby Blachman <abbyblachman@Abbys-MacBook-Pro.local>
2020-03-11 18:13:02 -0500

    server update

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ app.use(cors())
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("public"));
+  app.use(express.static("client/build"));
 }
 
 /*React root*/
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
   app.use(function(req, res, next) {
